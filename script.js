a7187dd75aa39dbd60e34708a3f16a0605e4c7ff
Nitis <nitis1203@gmail.com>
2024-01-02 16:56:45 +0530

    Updated Scroll effect

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     var body = document.querySelector("body");
+    var header = document.querySelector("header");
+    var headerImage = document.querySelector("header>img")
     var maxScroll = window.innerHeight;
+    var threshold = 30;
     window.addEventListener('scroll', function() {
         var scrollPosition = window.scrollY;
         var scrollPercentage = (scrollPosition / maxScroll) * 100;
-        if (scrollPercentage > 0) {
+        if (scrollPercentage >= 0 && scrollPercentage<=100) {
             body.style.transition = 'filter 0.5s ease-in-out';
-            body.style.backdropFilter = 'grayscale(' + (scrollPercentage) + '%)';
-        }
-        else {
-            body.style.transition = 'none';
-            body.style.backdropFilter = 'none';
+            body.style.backdropFilter = 'grayscale(' + (scrollPercentage) + '%) blur(' + (0.5+scrollPercentage/20) + 'px)';
+            header.style.backgroundColor = 'rgba(244, 206, 20,' + (0.2 + scrollPercentage/50) + ')';
         }
         if(scrollPosition<=this.window.innerHeight){
             body.style.backgroundSize = 100 + scrollPosition/40 + '%';
+        }
+        console.log(scrollPosition);
+        if(scrollPosition > 295 - threshold) {
+            header.classList.add('nav1Bar');
+            headerImage.classList.remove('nav2Bar')
+        }
+        else if(scrollPosition <= 295 - threshold){
+            header.classList.remove('nav1Bar')
+            headerImage.classList.add('nav2Bar');
         }
     });
 });
